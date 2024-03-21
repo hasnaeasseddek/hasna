6281b85ee9311f92ed60d12a623fc5148d56e066
Hasna ASSEDDEK <hasna.asseddek@jobintech-uir.ma>
2024-03-21 10:45:43 +0000

    freelance

--- auth/microservice.Auth/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace microservice.Auth.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string Name { get; set; }
-    }
-}
--- auth/microservice.Auth/service/Iservice/Itest.cs
+++
@@ -1,9 +0,0 @@
-﻿using microservice.Auth.Models;
-
-namespace microservice.Auth.service.Iservice
-{
-    public interface Itest
-    {
-        public string generateToken(ApplicationUser applicationuser);
-    }
-}
